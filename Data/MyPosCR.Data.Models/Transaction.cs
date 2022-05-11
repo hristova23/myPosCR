e62e1823e72f63091a617be16a3a736ea28e5bb0
Victoriya Hristova <victoriya.hristova04@gmail.com>
2022-05-11 18:57:33 +0300

    Initialize Data Layer

---
+++ Data/MyPosCR.Data.Models/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPosCR.Data.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Message { get; set; }
+        
+        public int SenderId { get; set; }
+        public User Sender { get; set; }
+        
+        public int RecieverId { get; set; }
+        public User Reciever { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Data/MyPosCR.Data/MyPosCRDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPosCR.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPosCR.Data
+{
+    internal class MyPosCRDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer("Server=DESKTOP-LTGBELC\\SQLEXPRESS;Database=MyPosCR;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<User>(user =>
+            {
+                user
+                    .HasMany(s => s.SendedTransactions)
+                    .WithOne(s => s.Sender)
+                    .HasForeignKey(s => s.SenderId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                user
+                    .HasMany(s => s.RecievedTransactions)
+                    .WithOne(s => s.Reciever)
+                    .HasForeignKey(s => s.RecieverId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            //builder.Entity<Transaction>(transaction =>
+            //{
+            //    transaction
+            //        .HasKey(sr => new { sr.SenderId, sr.RecieverId });
+
+            //    transaction
+            //        .HasOne(t => t.Sender)
+            //        .WithMany(t => t.Transactions)
+            //        .HasForeignKey(b => b.SenderId)
+            //        .OnDelete(DeleteBehavior.Restrict);
+
+            //    transaction
+            //        .HasOne(t => t.Reciever)
+            //        .WithMany(t => t.Transactions)
+            //        .HasForeignKey(b => b.RecieverId)
+            //        .OnDelete(DeleteBehavior.Restrict);
+            //});
+        }
+    }
+}
