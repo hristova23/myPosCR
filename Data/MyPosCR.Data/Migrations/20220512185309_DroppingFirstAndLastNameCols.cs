f3ab87efa1e499cb07423c863363503e423da5d5
Victoriya Hristova <victoriya.hristova04@gmail.com>
2022-05-12 21:58:42 +0300

    Add Migration

---
+++ Data/MyPosCR.Data/Migrations/20220512185309_DroppingFirstAndLastNameCols.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPosCR.Data.Migrations
+{
+    public partial class DroppingFirstAndLastNameCols : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
